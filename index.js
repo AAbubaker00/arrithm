aa89c8e8dd20997d47f19d66ecc4ee1fb5970614
AAbdelaziz00 <abdulabu016@gmail.com>
2023-10-04 12:43:43 +0100

    more solver methods

--- index.js
+++ index.js
@@ -5,47 +5,21 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+//* Custom packags
+const math_solver = require("./methods_collection/math_solver.js");
+const symbolab = require("./methods_collection/symbolab.js")
+
 const app = express();
 
-var _FixedEndPoint = "https://mathsolver.microsoft.com/en/solve-problem/";
-
 app.get("/", (req, res) => {
-  res.json("Welcome to me api");
+  res.sendfile("./templates/home.html");
 });
 
-function formatEquation(equ) {
-  equ = equ.replace(/\(/g, "{").replace(/\)/g, "}");
-  equ = equ.replace(/\\\\/g, "`");
-  equ = equ.replace(/sqrt/g, "\\sqrt");
-  equ = equ.replace(/ /g, "");
+app.get("/api/:latextformat", async (request, res) => {
 
-  return equ;
-}
+  // symbolab.solver(request.params.latextformat, cheerio, res)
 
-app.get("/api/v1/:latextformat", (request, res) => {
-  const expression = formatEquation(request.params.latextformat);
-
-  console.log(_FixedEndPoint + expression);
-
-  axios
-    .get(_FixedEndPoint + expression)
-    .then((response) => {
-      const $ = cheerio.load(response.data);
-      const scriptContent = $("script#__NEXT_DATA__", response.data).html();
-
-      // Check if scriptContent is not empty
-      if (scriptContent) {
-        // Parse the JSON content of the script tag
-        const jsonData = JSON.parse(scriptContent);
-
-        //   console.log(jsonData["props"]["pageProps"]["response"]["mathSolverResult"]["actions"]);
-
-        res.json(jsonData["props"]["pageProps"]["response"]["mathSolverResult"]["actions"]);
-      } else {
-        console.log("Script tag with ID '__NEXT_DATA__' not found or empty.");
-      }
-    })
-    .catch((error) => console.log(error));
+  math_solver.solver(request.params.latextformat, axios, cheerio, res);
 });
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
